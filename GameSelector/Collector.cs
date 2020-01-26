32de658e95a93d020b902d6f0b0092e83b5035d8
Velkas <treestump34@gmail.com>
2020-01-26 15:32:46 -0800

    Add project files.

---
+++ GameSelector/Collector.cs
@@ -0,0 +1,58 @@
+﻿using Gameloop.Vdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace GameSelector
+{
+    public static class Collector
+    {
+        public static List<string> GetLibraries(string libFile)
+        {
+            var libraries = new List<string>();
+
+            if (File.Exists(libFile))
+            {
+                dynamic steam = VdfConvert.Deserialize(File.ReadAllText(libFile));
+                var pattern = @"^\d$";
+                var rg = new Regex(pattern);
+                foreach (var item in steam.Value.Children())
+                {
+                    if (rg.IsMatch(item.Key))
+                    {
+                        libraries.Add(Path.Combine(item.Value.ToString(), "steamapps"));
+                    }
+                }
+            }
+
+
+            return libraries;
+        }
+
+        public static List<string> GetGames(List<string> libs)
+        {
+            var games = new List<string>();
+
+            foreach (var dir in libs)
+            {
+                if (Directory.Exists(dir))
+                {
+                    DirectoryInfo d = new DirectoryInfo(dir);
+                    FileInfo[] files = d.GetFiles("appmanifest*.acf");
+
+                    foreach (var file in files)
+                    {
+                        dynamic info = VdfConvert.Deserialize(File.ReadAllText(file.FullName));
+                        games.Add(info.Value.name.ToString());
+                    }
+                }
+            }
+
+            return games;
+        }
+
+    }
+}
---
+++ GameSelector/Entry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace GameSelector
+{
+    class Entry
+    {
+        static readonly Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            var libFile = Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles(x86)"), "steam", "steamapps", "libraryfolders.vdf");
+            var libs = Collector.GetLibraries(libFile);
+            libs.Add(Path.Combine(Environment.GetEnvironmentVariable("ProgramFiles(x86)"), "steam", "steamapps"));
+
+            var games = Collector.GetGames(libs);
+
+            var exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("Options:\n\t ENTER: Select a random steam game\n\tESCAPE: exit");
+                var input = Console.ReadKey();
+
+                if (input.Key == ConsoleKey.Enter)
+                {
+                    int r = rnd.Next(games.Count);
+                    Console.WriteLine(games[r]);
+                    input = Console.ReadKey();
+                }
+
+                if (input.Key == ConsoleKey.Escape)
+                {
+                    exit = true;
+                }
+            }
+        }
+    }
+}
